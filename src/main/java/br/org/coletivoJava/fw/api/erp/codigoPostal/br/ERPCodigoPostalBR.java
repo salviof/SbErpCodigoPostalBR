c3e47e38d001b9db2fedfdb570d44825dc22cf74
Salvio Furbino <salviof@gmail.com>
2019-10-17 11:08:36 -0300

    Atualizacao automática

---
+++ src/main/java/br/org/coletivoJava/fw/api/erp/codigoPostal/br/ERPCodigoPostalBR.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.coletivoJava.fw.api.erp.codigoPostal.br;
+
+/**
+ *
+ * @author desenvolvedorninja01
+ */
+import com.super_bits.modulosSB.SBCore.modulos.erp.ApiERPColetivoJavaFW;
+import com.super_bits.modulosSB.SBCore.modulos.erp.ItfApiErpSuperBits;
+
+@ApiERPColetivoJavaFW(descricaoApi = "Codigo Postal BR", nomeApi = "CodigoPostalBR", slugInicial = "GatewayPgto")
+public enum ERPCodigoPostalBR implements ItfApiErpSuperBits<ItfCodigoPostalBR> {
+
+    REPUBLICA_VIRUTAL,
+    BANCO_LOCAL;
+
+    @Override
+    public Class<? extends ItfCodigoPostalBR> getInterface() {
+        return ItfCodigoPostalBR.class;
+    }
+
+}
---
+++ src/main/java/br/org/coletivoJava/fw/api/erp/codigoPostal/br/ItfCodigoPostalBR.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.org.coletivoJava.fw.api.erp.codigoPostal.br;
+
+/**
+ * @author desenvolvedorninja01
+ * @since 16/10/2019 
+ * @version 1.0 
+ */
+public interface ItfCodigoPostalBR {
+
+}
